72cad0c10cbf86e18088c82f468eec63644e6031
Matt Schoenbauer <MattSchoenbauer@matts-mbp.dhcp.nd.edu>
2018-12-08 21:19:42 -0500

    simulator update, moving points

--- point_mass.cpp
+++ point_mass.cpp
@@ -1,8 +1,9 @@
 // point_mass.cpp
 
 #include "point_mass.h"
-
-#include<cmath>
+#include <iostream>
+#include "gfx3.h"
+#include <cmath>
 #define GG 1
 
 using namespace std;
@@ -36,7 +37,7 @@ void Mass::setMass(XPoint center, double mass, double radius, Vect veloc, Vect a
 
 XPoint Mass::getCenter() { return center; }
 double Mass::getRadius() { return radius; }
-double Mass::getMass() { return mass; }
+double Mass::getMa() { return mass; }
 Vect Mass::getVelocity() { return veloc; }
 Vect Mass::getAcceleration() { return accel; }
 
@@ -45,23 +46,27 @@ void Mass::draw(void){
 	double average_mass = 40;
 	double average_volume = M_PI * pow(average_radius,2);
 	double average_density = average_mass/average_volume;
-	int color_max = 225;
+	int max_color = 225;
 	double volume = M_PI * pow(getRadius(),2);
 	double density = getMa()/volume;
 	gfx_color(max_color * (density/average_density)/(1 + density/average_density),0,max_color * 1/(1 + density/average_density));
 	XPoint center = getCenter();
-	gfx_fill_circle(center.x,center.y,getRadius);
+	gfx_fill_circle(center.x,center.y,getRadius());
 }
 
 void Mass::update(void){
 	XPoint c = getCenter();
 	Vect v = getVelocity(), a = getAcceleration();
-	c.x = c.x + v.x;
-	c.y = c.y + v.y;
-	v.x = v.x + a.x;
-	v.y = v.y + a.y;
-	setVelocity(v);
+//	cout << "Position is " << c.x << "," << c.y << endl;
+//	cout << "Velocity is " << v.x << "," << v.y << endl;
+	c.x = c.x + v.x/20.;
+	c.y = c.y + v.y/20.;
+	v.x = v.x + a.x/20.;
+	v.y = v.y + a.y/20.;
 	setCenter(c);
+	setVeloc(v);
+//	cout << "Position is " << c.x << "," << c.y << endl;
+//	cout << "Velocity is " << v.x << "," << v.y << endl;
 }
 
 void Mass::merge(Mass m){
@@ -72,8 +77,8 @@ void Mass::merge(Mass m){
 	c.y = (c.y * getMa() + cc.y * m.getMa())/(new_mass);
 	v.x = (v.x * getMa() + vv.x * m.getMa())/(new_mass);
 	v.y = (v.y * getMa() + vv.y * m.getMa())/(new_mass);
-	setRadius(sqrt(pow(getRadius,2) + pow(m.getRadius(),2)));
-	setVelocity(v);
+	setRadius(sqrt(pow(getRadius(),2) + pow(m.getRadius(),2)));
+	setVeloc(v);
 	setCenter(c);
 	setMa(new_mass);
 }
