61222302f15baff0bb365efd5fd21c9ba867793c
Matt Schoenbauer <MattSchoenbauer@matts-mbp.dhcp.nd.edu>
2018-12-09 17:53:31 -0500

    point_mass final update

--- point_mass.cpp
+++ point_mass.cpp
@@ -41,6 +41,9 @@ double Mass::getMa() { return mass; }
 Vect Mass::getVelocity() { return veloc; }
 Vect Mass::getAcceleration() { return accel; }
 
+//This function draws our point masses.
+//The denisty is diplayed by the color of the mass.
+//More dense objects are red, less dense objects are blue
 void Mass::draw(void){
 	double average_radius = 40;
 	double average_mass = 100;
@@ -51,28 +54,24 @@ void Mass::draw(void){
 	double density = getMa()/volume;
 	gfx_color(max_color * (density/average_density)/(1 + density/average_density),0,max_color * 1/(1 + density/average_density));
 	Vect center = getCenter();
-//	cout << getRadius() << endl;
-//	gfx_circle(center.x,center.y,getRadius());
 	gfx_fill_circle(center.x,center.y,getRadius());
 	gfx_flush();
 }
 
+//Updates the position and velocity based on the acceleration
 void Mass::update(void){
 	Vect c = getCenter();
 	Vect v = getVelocity(), a = getAcceleration();
-//	cout << "Position is " << c.x << "," << c.y << endl;
-//	cout << "Velocity is " << v.x << "," << v.y << endl;
-	double delta_t = .05;
+	double delta_t = .05;//Increasing this value gives slower but more accurate simulations
 	c.x = c.x + delta_t*v.x + pow(delta_t,2)*a.x/2.;
 	c.y = c.y + delta_t*v.y + pow(delta_t,2)*a.y/2.;
 	v.x = v.x + delta_t*a.x;
 	v.y = v.y + delta_t*a.y;
 	setCenter(c);
 	setVeloc(v);
-//	cout << "Position is " << c.x << "," << c.y << endl;
-//	cout << "Velocity is " << v.x << "," << v.y << endl;
 }
 
+//Note that momentum and center of mass are preserved when two objects collide
 void Mass::merge(Mass m){
 	double new_mass = getMa() + m.getMa();
 	Vect c = getCenter(), cc = m.getCenter();
@@ -87,11 +86,13 @@ void Mass::merge(Mass m){
 	setMa(new_mass);
 }
 
+//This configues in the contribution of gravity from one point onto another
 void Mass::add_acceleration(Mass m){
 	float dist = distance(m);
 	setAccel((getCenter() + m.getCenter() * (-1))*(-GG*m.getMa()/pow(dist,3)) + getAcceleration());
 }
 
+//returns distance between two points
 double Mass::distance(Mass m){
     Vect c1 = getCenter(), c2 = m.getCenter();
     return sqrt(pow(c1.x - c2.x,2) + pow(c1.y-c2.y,2));
