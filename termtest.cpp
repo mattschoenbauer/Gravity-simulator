a8ffee44bb19996f5a4a5cec7eb62c7fe09fee5a
Kyle J Duffy <kylejduffy@kyles-mbp.dhcp.nd.edu>
2018-12-08 21:10:06 -0500

    now tests prompts

--- termtest.cpp
+++ termtest.cpp
@@ -3,19 +3,21 @@
 #include "terminal.h"
 #include <X11/Xlib.h>
 #include <unistd.h>
+#include <iostream> // for testing
 #include "gfx3.h"
 using namespace std;
 
 int main() {
 	const int w = 500, h = 500;
 	gfx_open(w,h, "Terminal Test");
+	string test = "testt";
+	test += '\b';
+	cout << test.c_str() << endl;
 
 	Terminal term(w,h);
 	while (true) {
-		int e = (int)gfx_wait();
-		if (e == 27) break;
-		else if (e == ' ') term.print("Hello this is a test");
-		usleep(10000);
+		string entry = term.prompt("enter something: ");
+		cout << entry << endl;
 	}
 	return 0;
 
