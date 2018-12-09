5373f8d8321bb78690d82a9fd5529635531f9853
Kyle J Duffy <kylejduffy@kyles-mbp.dhcp.nd.edu>
2018-12-09 14:12:48 -0500

    randdom initializers, WIP restart function

--- simulator.cpp
+++ simulator.cpp
@@ -17,6 +17,7 @@ using namespace std;
 
 const int wid=gfx_screenwidth();
 const int ht=gfx_screenheight();
+const int mrgn=gfx_screenheight()/20;
 // Terminal term(wid/2, ht/2);
 
 void interactive_initialize(list<Mass>&);
@@ -29,7 +30,237 @@ void random_small_initialize(list<Mass>&);
 void random_large_initialize(list<Mass>&);
 void grid_initialize(list<Mass>&);
 void ring_initialize(list<Mass>&);
+Mass random(int,int,int,int);
 
+int main(){
+	srand(time(NULL));
+	string option;
+	gfx_open(wid,ht,"Gravity Simulator");
+	list<Mass> masslist;
+	init(masslist);
+
+	int n = 0;
+	while(true) {
+		draw_points(masslist);
+		update(masslist);
+		if(gfx_event_waiting()){
+			n = gfx_wait();
+			if (n == (int)'q') break;
+			if (n == (int)'r') { // WIP
+				main();
+			}
+		}
+		usleep(40000);
+		gfx_clear();
+	}
+
+	return 0;
+}
+
+void init(list<Mass>& masslist) {
+	static Terminal term(wid/2, ht/2);
+	string entry = term.prompt("(i)nput initial condions, or (l)oad file? ", 'b');
+	if (entry.length() > 1) init(masslist);
+	else {
+		char e = entry[0];
+		if (e == 'i') interactive_initialize(masslist);
+		else if (e == 'l') batch_initialize(masslist);
+		else init(masslist);
+	}
+}
+
+void interactive_initialize(list<Mass>& masslist) {
+    int wid = gfx_screenwidth();
+    int ht = gfx_screenheight();
+    Terminal term(wid/2, ht/2); // configurable... account for desired size
+
+    string msg = "Screen dimension are " + to_string(wid) + " x " + to_string(ht) + ".";
+    term.print(msg.c_str());
+    bool finished = false;
+    char c;
+    int count = 1;
+    Vect center, v, a(0,0);
+    double cx,cy,mass,radius;
+    while (!finished) {
+        term.print("Object " + to_string(count++)); // unsure if ++ comes after method call in precedence, I think this should work...
+        cx = atof(term.prompt("Input X Position Coordinate: ", 'd').c_str());
+        cy = atof(term.prompt("Input Y Position Coordinate: ", 'd').c_str());
+        center.x = cx + wid/2;
+        center.y = cy + ht/2;
+        mass = atof(term.prompt("Input Mass: ", 'd').c_str());
+        radius = atof(term.prompt("Input Radius: ", 'd').c_str());
+        v.x = atof(term.prompt("Input X Velocity Coordinate: ", 'd').c_str());
+        v.y = atof(term.prompt("Input Y Velocity Coordinate: ", 'd').c_str());
+        /* a.x = atof(term.prompt("Input X Acceleration Coordinate: ", 'd').c_str()); */
+        /* a.y = atof(term.prompt("Input Y Acceleration Coordinate: ", 'd').c_str()); */
+
+        Mass m(center, mass, radius, v, a);
+        masslist.push_back(m);
+		m.draw();
+        c = term.prompt("Finished (y/n)? ", 'b')[0];
+        if (c == 'y') finished = true;
+    }
+}
+
+void batch_initialize(list<Mass>& masslist) {
+    Terminal term(wid/2, ht/2);
+	string filename = term.prompt("Enter a filename (or rand for random): ", 'b');//second command will be filename
+	if (filename == "randsmall") {
+		random_small_initialize(masslist);
+		return;
+	}
+	if (filename == "randlarge") {
+		random_large_initialize(masslist);
+		return;
+	}
+	if (filename == "grid") {
+		grid_initialize(masslist);
+		return;
+	}
+	if (filename == "ring") {
+		ring_initialize(masslist);
+		return;
+	}
+	ifstream ifs;
+	ifs.open(filename);
+	while(!ifs){
+		term.print("Please input a valid file name");//Checking for a valid filename
+		filename = term.prompt("Try again: ", 'b');
+		ifs.open(filename);
+	}
+	batch_initialize(masslist,ifs);
+	//		for(Mass m : masslist){ cout << m.getRadius() << endl;}
+}
+
+void batch_initialize(list<Mass>& masslist, istream& ifs){
+	int wid=gfx_screenwidth();
+	int ht=gfx_screenheight();
+	double cx, cy, mass, radius;
+	Vect center, v, a(0,0);
+	ifs >> cx;
+	while(!ifs.eof()){
+		ifs >> cy;
+		center.x = cx + wid /2;
+		center.y = cy + ht /2;
+		ifs >> mass;
+		ifs >> radius;
+		ifs >> v.x;
+		ifs >> v.y;
+		Mass m(center,mass,radius,v,a);
+		masslist.push_back(m);
+		ifs >> cx;
+	}
+}
+
+void random_large_initialize(list<Mass>& masslist) {
+	const int num = 20;
+	for (int i = 0; i < num; i++) {
+		masslist.push_back(random(10,30,75,125));
+	}
+}
+
+void random_small_initialize(list<Mass>& masslist) {
+	const int num = 40;
+	for (int i = 0; i < num; i++) {
+		masslist.push_back(random(1,5,30,50));
+	}
+}
+
+Mass random(int r_min, int r_max, int m_min, int m_max) {
+	Vect c,v(0,0),a(0,0);
+	double r = (double)(r_min + (rand()%(r_max-r_min+1)));
+	double m = (double)(m_min + (rand()%(m_max-m_min+1)));
+	c.x = (double)(mrgn + (int)r + (rand()%(wid-2*mrgn-(int)r+1)));
+	c.y = (double)(mrgn + (int)r + (rand()%(ht-2*mrgn-(int)r+1)));
+	return Mass(c,m,r,v,a);
+}
+
+void grid_initialize(list<Mass>& masslist) {
+}
+
+void ring_initialize(list<Mass>& masslist) {
+}
+
+void draw_points(list<Mass> masslist){
+	for(Mass m : masslist){m.draw();}
+}
+
+void update(list<Mass>& masslist){
+	for(auto itr = masslist.begin(); itr != masslist.end(); itr++){
+		for(auto itr2 = next(itr,1); itr2 != masslist.end(); itr2++){
+			if((*itr).distance(*itr2) <= (*itr).getRadius() + (*itr2).getRadius()){
+				(*itr).merge(*itr2);
+				masslist.erase(itr2);
+			}
+		}
+	}
+//	int count = 1;
+	//Now computing accelerations
+	Vect zero(0,0);
+	for(auto itr = masslist.begin(); itr != masslist.end(); itr++){
+		(*itr).setAccel(zero);
+		for(auto itr2 = masslist.begin(); itr2 != masslist.end(); itr2++){
+			if(itr != itr2){
+				(*itr).add_acceleration(*itr2);
+			}
+		}
+	}
+	for(auto itr = masslist.begin(); itr != masslist.end(); itr++){
+//		cout << "Updating Point " << count << endl;
+		(*itr).update();
+//		count++;
+	}
+}
+
+//void interactive_initialize(list<Mass>& masslist){
+//	int wid=gfx_screenwidth();
+//	int ht=gfx_screenheight();
+//	cout << "Screen dimension are " << wid << " x " << ht << ".\n";
+//	bool finished = false;
+//	char c;
+//	int count = 1;
+//	double cx, cy, vx, vy, ax, ay, mass, radius;
+//	Vect center;
+////	Vect accel(0,0);
+//	while(!finished){
+//		cout << "Object " << count << endl << endl;
+//		cout << "Input X Position Coordinate: ";
+//		cin >> cx;
+//		cout << endl;
+//		cout << "Input Y Position Coordinate: ";
+//		cin >> cy;
+//		cout << endl;
+//		center.x = cx+ wid /2;
+//		center.y = cy+ ht /2;
+//		cout << "Input Mass: ";
+//		cin >> mass;
+//		cout << endl;
+//		cout << "Input Radius: ";
+//		cin >> radius;
+//		cout << endl;
+//		cout << "Input X Velocity Coordinate: ";
+//		cin >> vx;
+//		cout << endl;
+//		cout << "Input Y Velocity Coordinate: ";
+//		cin >> vy;
+//		Vect veloc(vx,vy);
+//		cout << endl;
+//		cout << "Input X Acceleration Coordinate: ";
+//		cin >> ax;
+//		cout << endl;
+//		cout << "Input Y Acceleration Coordinate: ";
+//		cin >> ay;
+//		Vect accel(ax,ay);
+//		cout << endl;
+//		Mass m(center,mass,radius,veloc,accel);
+//		count++;
+//		masslist.push_back(m);
+//		cout << "Finished? ";
+//		cin >> c;
+//		cout << endl;
+//		if(c == 'y'){finished = true;}
+//	}
+//}
 /*
 int main(int argc, char *argv[]){
 
@@ -91,212 +322,4 @@ int main(int argc, char *argv[]){
 }
 */
 
-int main(int argc, char *argv[]){
-	string option;
-	gfx_open(wid,ht,"Gravity Simulator");
-	list<Mass> masslist;
-	init(masslist);
 
-	int n = 0;
-	while(true) {
-		draw_points(masslist);
-		update(masslist);
-		if(gfx_event_waiting()){
-			n = gfx_wait();
-			if (n == (int)'q') break;
-		}
-		usleep(40000);
-		gfx_clear();
-	}
-
-	return 0;
-}
-
-void init(list<Mass>& masslist) {
-	static Terminal term(wid/2, ht/2);
-	string entry = term.prompt("(i)nput initial condions, or (l)oad file? ", 'b');
-	if (entry.length() > 1) init(masslist);
-	else {
-		char e = entry[0];
-		if (e == 'i') interactive_initialize(masslist);
-		else if (e == 'l') batch_initialize(masslist);
-		else init(masslist);
-	}
-}
-
-void interactive_initialize(list<Mass>& masslist) {
-    int wid = gfx_screenwidth();
-    int ht = gfx_screenheight();
-    Terminal term(wid/2, ht/2); // configurable... account for desired size
-
-    string msg = "Screen dimension are " + to_string(wid) + " x " + to_string(ht) + ".";
-    term.print(msg.c_str());
-    bool finished = false;
-    char c;
-    int count = 1;
-    Vect center, v, a(0,0);
-    double cx,cy,mass,radius;
-    while (!finished) {
-        term.print("Object " + to_string(count++)); // unsure if ++ comes after method call in precedence, I think this should work...
-        cx = atof(term.prompt("Input X Position Coordinate: ", 'd').c_str());
-        cy = atof(term.prompt("Input Y Position Coordinate: ", 'd').c_str());
-        center.x = cx + wid/2;
-        center.y = cy + ht/2;
-        mass = atof(term.prompt("Input Mass: ", 'd').c_str());
-        radius = atof(term.prompt("Input Radius: ", 'd').c_str());
-        v.x = atof(term.prompt("Input X Velocity Coordinate: ", 'd').c_str());
-        v.y = atof(term.prompt("Input Y Velocity Coordinate: ", 'd').c_str());
-        /* a.x = atof(term.prompt("Input X Acceleration Coordinate: ", 'd').c_str()); */
-        /* a.y = atof(term.prompt("Input Y Acceleration Coordinate: ", 'd').c_str()); */
-
-        Mass m(center, mass, radius, v, a);
-        masslist.push_back(m);
-		m.draw();
-        c = term.prompt("Finished (y/n)? ", 'b')[0];
-        if (c == 'y') finished = true;
-    }
-}
-
-void batch_initialize(list<Mass>& masslist) {
-    Terminal term(wid/2, ht/2);
-	string filename = term.prompt("Enter a filename (or rand for random): ", 'b');//second command will be filename
-	if (filename == "rand_small") {
-		random_small_initialize(masslist);
-		return;
-	}
-	if (filename == "rand_large") {
-		random_large_initialize(masslist);
-		return;
-	}
-	if (filename == "grid") {
-		grid_initialize(masslist);
-		return;
-	}
-	if (filename == "ring") {
-		ring_initialize(masslist);
-		return;
-	}
-	ifstream ifs;
-	ifs.open(filename);
-	while(!ifs){
-		term.print("Please input a valid file name");//Checking for a valid filename
-		filename = term.prompt("Try again: ", 'b');
-		ifs.open(filename);
-	}
-	batch_initialize(masslist,ifs);
-	//		for(Mass m : masslist){ cout << m.getRadius() << endl;}
-}
-
-
-//void interactive_initialize(list<Mass>& masslist){
-//	int wid=gfx_screenwidth();
-//	int ht=gfx_screenheight();
-//	cout << "Screen dimension are " << wid << " x " << ht << ".\n";
-//	bool finished = false;
-//	char c;
-//	int count = 1;
-//	double cx, cy, vx, vy, ax, ay, mass, radius;
-//	Vect center;
-////	Vect accel(0,0);
-//	while(!finished){
-//		cout << "Object " << count << endl << endl;
-//		cout << "Input X Position Coordinate: ";
-//		cin >> cx;
-//		cout << endl;
-//		cout << "Input Y Position Coordinate: ";
-//		cin >> cy;
-//		cout << endl;
-//		center.x = cx+ wid /2;
-//		center.y = cy+ ht /2;
-//		cout << "Input Mass: ";
-//		cin >> mass;
-//		cout << endl;
-//		cout << "Input Radius: ";
-//		cin >> radius;
-//		cout << endl;
-//		cout << "Input X Velocity Coordinate: ";
-//		cin >> vx;
-//		cout << endl;
-//		cout << "Input Y Velocity Coordinate: ";
-//		cin >> vy;
-//		Vect veloc(vx,vy);
-//		cout << endl;
-//		cout << "Input X Acceleration Coordinate: ";
-//		cin >> ax;
-//		cout << endl;
-//		cout << "Input Y Acceleration Coordinate: ";
-//		cin >> ay;
-//		Vect accel(ax,ay);
-//		cout << endl;
-//		Mass m(center,mass,radius,veloc,accel);
-//		count++;
-//		masslist.push_back(m);
-//		cout << "Finished? ";
-//		cin >> c;
-//		cout << endl;
-//		if(c == 'y'){finished = true;}
-//	}
-//}
-
-void batch_initialize(list<Mass>& masslist, istream& ifs){
-	int wid=gfx_screenwidth();
-	int ht=gfx_screenheight();
-	double cx, cy, mass, radius;
-	Vect center, v, a(0,0);
-	ifs >> cx;
-	while(!ifs.eof()){
-		ifs >> cy;
-		center.x = cx + wid /2;
-		center.y = cy + ht /2;
-		ifs >> mass;
-		ifs >> radius;
-		ifs >> v.x;
-		ifs >> v.y;
-		Mass m(center,mass,radius,v,a);
-		masslist.push_back(m);
-		ifs >> cx;
-	}
-}
-
-void random_large_initialize(list<Mass>& masslist) {
-}
-
-void random_small_initialize(list<Mass>& masslist) {
-}
-
-void grid_initialize(list<Mass>& masslist) {
-}
-
-void ring_initialize(list<Mass>& masslist) {
-}
-
-void draw_points(list<Mass> masslist){
-	for(Mass m : masslist){m.draw();}
-}
-
-void update(list<Mass>& masslist){
-	for(auto itr = masslist.begin(); itr != masslist.end(); itr++){
-		for(auto itr2 = next(itr,1); itr2 != masslist.end(); itr2++){
-			if((*itr).distance(*itr2) <= (*itr).getRadius() + (*itr2).getRadius()){
-				(*itr).merge(*itr2);
-				masslist.erase(itr2);
-			}
-		}
-	}
-//	int count = 1;
-	//Now computing accelerations
-	Vect zero(0,0);
-	for(auto itr = masslist.begin(); itr != masslist.end(); itr++){
-		(*itr).setAccel(zero);
-		for(auto itr2 = masslist.begin(); itr2 != masslist.end(); itr2++){
-			if(itr != itr2){
-				(*itr).add_acceleration(*itr2);
-			}
-		}
-	}
-	for(auto itr = masslist.begin(); itr != masslist.end(); itr++){
-//		cout << "Updating Point " << count << endl;
-		(*itr).update();
-//		count++;
-	}
-}
