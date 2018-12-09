c48e3a532010796c042c92637c739619b3b8d5ff
Kyle J Duffy <kylejduffy@kyles-mbp.dhcp.nd.edu>
2018-12-08 20:00:09 -0500

    next suite has some typing characteristics

--- terminal.cpp
+++ terminal.cpp
@@ -1,6 +1,7 @@
 // terminal.cpp
 
 #include "terminal.h"
+#include <iostream>
 
 Terminal::~Terminal() {}
 
@@ -12,16 +13,41 @@ Terminal::Terminal(int w, int h) {
 
 void Terminal::set_font_size(short s) { font_size = s; }
 int Terminal::get_font_size(char* text) { return gfx_textpixelheight(text, font); }
+int Terminal::get_width(char* text) { return gfx_textpixelwidth(text, font); }
 
 void Terminal::print(char* text) {
+	int row = prep_term(text);
+	gfx_text(0, row, text);
+}
+
+string Terminal::prompt(char* msg) {
+	string entry;
+	int row = prep_term(msg);
+	int col = get_width(msg);
+	int pt = get_font_size(msg);
+	do {
+		int e = (int)gfx_wait();
+		cout << (char)e << endl;
+		if (e == 13) break;
+		if(isalpha(e) || isdigit(e)) entry += (char)e;
+		gfx_color(tbgR,tbgG,tbgB);
+		gfx_fill_rectangle(row-pt,0, w, pt);
+		gfx_color(255,255,255);
+		gfx_text(0,row,msg);
+		gfx_text(col, row, entry.c_str());
+	} while (true);
+	return entry;
+}
+
+int Terminal::prep_term(char* text) {
 	static int printY = get_font_size(text);
 	gfx_color(255,255,255);
 	if (printY > h) {
 		terminal_clear();
 		printY = 0;
 	}
-	gfx_text(0, printY, text);
 	printY += get_font_size(text);
+	return printY - get_font_size(text);
 }
 
 void Terminal::terminal_clear() {
--- terminal.h
+++ terminal.h
@@ -4,6 +4,7 @@
 #define TERMINAL_H
 
 #include <X11/Xlib.h>
+#include <string>
 #include "gfx3.h"
 using namespace std;
 
@@ -12,14 +13,17 @@ class Terminal {
 		~Terminal();
 		Terminal(int w, int h);
 		void print(char* text);
+		string prompt(char* msg);
 		void set_font_size(short);
 		void terminal_clear();
 		int get_font_size(char* text);
+		int get_width(char* text);
 	private:
 		char* font = "-misc-fixed-medium-r-semicondensed--12-110-75-75-c-60-iso8859-1";
 		short font_size;
 		int w, h;
 		const int tbgR = 0, tbgG = 0, tbgB = 0;
+		int prep_term(char* text);
 };
 
 #endif
